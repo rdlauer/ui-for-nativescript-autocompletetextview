2ea273b4b4ed384bd073ef78b3508ab00cbd9d8c
Rob Lauer <rdlauer@gmail.com>
2017-01-09 13:53:32 -0600

    testing images on android

--- app/main-page.js
+++ app/main-page.js
@@ -10,34 +10,34 @@ exports.pageLoaded = function(args) {
 	page = args.object;
 	page.bindingContext = pageData;
 
-	var artists = ["Arcade Fire", "Bon Iver", "Daft Punk", "Elbow"];
+	//var artists = ["Arcade Fire", "Bon Iver", "Daft Punk", "Elbow"];
 
-	// var artists = [
-	// 	{
-	// 		text: "Arcade Fire",
-	// 		image: "~/images/arcade-fire.png"
-	// 	},
-	// 	{
-	// 		text: "Bon Iver",
-	// 		image: "~/images/bon-iver.png"
-	// 	},
-	// 	{
-	// 		text: "Daft Punk",
-	// 		image: "~/images/daft-punk.png"
-	// 	},
-	// 	{
-	// 		text: "Elbow",
-	// 		image: "~/images/elbow.png"
-	// 	}
-	// ];
-
-	for (var i = 0; i < artists.length; i++) {
-		items.push(new autocompleteModule.TokenModel(artists[i]));
-	};
+	var artists = [
+		{
+			text: "Arcade Fire",
+			image: "~/images/arcade-fire.png"
+		},
+		{
+			text: "Bon Iver",
+			image: "~/images/bon-iver.png"
+		},
+		{
+			text: "Daft Punk",
+			image: "~/images/daft-punk.png"
+		},
+		{
+			text: "Elbow",
+			image: "~/images/elbow.png"
+		}
+	];
 
 	// for (var i = 0; i < artists.length; i++) {
-	// 	items.push(new autocompleteModule.TokenModel(artists[i].text, artists[i].image));
+	// 	items.push(new autocompleteModule.TokenModel(artists[i]));
 	// };
+
+	for (var i = 0; i < artists.length; i++) {
+		items.push(new autocompleteModule.TokenModel(artists[i].text, artists[i].image));
+	};
 
 	pageData.set("items", items);
 };
